133f24f28e62e87a6d86a2d1dee54d46805d5c10
Stephen Epps <stephen.epps@mapzen.com>
2017-02-13 11:35:33 -0800

    Formatting and testing statement additions

--- tests/test_belongsto.js
+++ tests/test_belongsto.js
@@ -1,4 +1,6 @@
-var get_record = require('./get_record.js')
+var get_record = require('../utils/get_record.js')
+var successes = 0
+var failures = 0
 // pulling everything together
 function condition(recordid, belongsto_ids)
 {
@@ -9,10 +11,18 @@ function condition(recordid, belongsto_ids)
     }
     if (belongsto_ids.every(belongsto)) {
     	console.log('WOOHOO')
+    	successes ++  
     }
     	else {console.log('BOOHOO')
+    	failures ++
     }
 }
 
-condition(1108730699, [102191581, 85633287, 85688877])
-
+module.exports.run = function()
+{
+	successes = 0
+	failures = 0
+	console.log('Test: belongsto')
+	condition(1108730699, [102191581, 85633287, 85688877])
+	console.log('Successes:', successes, 'Failures:', failures)
+}
